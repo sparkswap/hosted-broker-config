964bafe0a3bc9306bba339e80011b5386f676fe8
Trey Griffith <trey.griffith@gmail.com>
2018-06-26 16:10:16 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const query = require('micro-query')
+const request = require('request-promise-native')
+
+const TEMPLATE_FILE_LOCATION = 'https://raw.githubusercontent.com/kinesis-exchange/broker-cli/master/sample-.kcli.js'
+let template = ''
+
+const DEFAULT_SERVER = 'localhost'
+
+module.exports = async (req) => {
+  if (!template) template = await request(TEMPLATE_FILE_LOCATION)
+
+  const server = query(req).server || DEFAULT_SERVER
+  return template.replace("// rpcAddress: 'localhost:27492'", `rpcAddress: '${server}:27492'`)
+}
